82146034ef06ebcf04332f16ce18509fadf1ab75
Elisha Fitch-Cook <elisha@elishacook.com>
2015-03-30 12:47:47 -0400

    Changed to new name in unit tests

--- test_simpleobserver.py
+++ test_simpleobserver.py
@@ -1,12 +1,12 @@
 import unittest
 from mock import Mock
-import observer
+import simpleobserver
 
 class TestSubject(unittest.TestCase):
 
 	def test_invalid_event(self):
 		"""Raises an error if attempting to add a listener to an unregistered event"""
-		subject = observer.Subject()
+		subject = simpleobserver.Subject()
 
 		with self.assertRaises(AssertionError):
 			subject('foo', None)
@@ -16,7 +16,7 @@ class TestSubject(unittest.TestCase):
 		"""Can add a listener to an allowed event"""
 		foo_listener = Mock()
 		bar_listener = Mock()
-		subject = observer.Subject('foo', 'bar', 'baz')
+		subject = simpleobserver.Subject('foo', 'bar', 'baz')
 		subject('foo', foo_listener)
 		subject('bar', bar_listener)
 
